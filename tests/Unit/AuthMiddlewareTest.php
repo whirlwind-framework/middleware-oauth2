96ffe72849285f5617cbd6e599dca0c6811c8078
Artem Tsyganov <ontroip@gmail.com>
2022-03-28 22:17:52 +0300

    Improvements - uncommented tests

--- tests/Unit/AuthMiddlewareTest.php
+++ tests/Unit/AuthMiddlewareTest.php
@@ -18,183 +18,183 @@ use Whirlwind\Middleware\OAuth\TokenInfoRepository;
 
 class AuthMiddlewareTest extends TestCase
 {
-//    public function testWithoutAuthorizationHeader(): void
-//    {
-//        $requestFactory = new ServerRequestFactory();
-//        $request = $requestFactory->createServerRequest('POST', '/some/uri');
-//        $handler = $this->createMock(RequestHandlerInterface::class);
-//        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
-//        $middleware = new AuthMiddleware($tokenInfoRepository);
-//
-//        $this->expectException(UnauthorizedException::class);
-//        $middleware->process($request, $handler);
-//    }
-//
-//    public function testWithWrongScopes(): void
-//    {
-//        $requestFactory = new ServerRequestFactory();
-//        $handler = $this->createMock(RequestHandlerInterface::class);
-//        $request = $requestFactory->createServerRequest('POST', '/some/uri')
-//            ->withHeader('Authorization', 'Bearer r32rf3rf32');
-//        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
-//        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
-//            ->willReturn(new TokenInfo('', '', '', '', ['events.delivery' => []]));
-//
-//        $middleware = (new AuthMiddleware($tokenInfoRepository))
-//            ->withRequiredScopes(['trips.example']);
-//        $this->expectException(ForbiddenHttpException::class);
-//        $middleware->process($request, $handler);
-//    }
-//
-//    public function testWithGlobalScope(): void
-//    {
-//        $requestFactory = new ServerRequestFactory();
-//        $handler = $this->createMock(RequestHandlerInterface::class);
-//        $defaultSuccessResponse = $this->createMock(ResponseInterface::class);
-//
-//        $defaultSuccessResponse->method('getStatusCode')
-//            ->willReturn(200);
-//        $handler->method('handle')
-//            ->willReturn($defaultSuccessResponse);
-//
-//        $request = $requestFactory->createServerRequest('POST', '/some/uri')
-//            ->withHeader('Authorization', 'Bearer r32rf3rf32');
-//
-//        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
-//        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
-//            ->willReturn(new TokenInfo('', '', '', '', ['events' => []]));
-//
-//        $middleware = (new AuthMiddleware($tokenInfoRepository))
-//            ->withRequiredScopes(['events.example']);
-//        $actual = $middleware->process($request, $handler);
-//
-//        $this->assertEquals(200, $actual->getStatusCode());
-//    }
-//
-//    public function testWithoutRequiredScopes(): void
-//    {
-//        $requestFactory = new ServerRequestFactory();
-//        $handler = $this->createMock(RequestHandlerInterface::class);
-//        $defaultSuccessResponse = $this->createMock(ResponseInterface::class);
-//
-//        $defaultSuccessResponse->method('getStatusCode')
-//            ->willReturn(200);
-//        $handler->method('handle')
-//            ->willReturn($defaultSuccessResponse);
-//
-//        $request = $requestFactory->createServerRequest('POST', '/some/uri')
-//            ->withHeader('Authorization', 'Bearer r32rf3rf32');
-//
-//        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
-//        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
-//            ->willReturn(new TokenInfo('', '', '', '', ['events' => []]));
-//
-//        $middleware = new AuthMiddleware($tokenInfoRepository);
-//        $actual = $middleware->process($request, $handler);
-//
-//        $this->assertEquals(200, $actual->getStatusCode());
-//    }
-//
-//    public function testWithClientException(): void
-//    {
-//        $requestFactory = new ServerRequestFactory();
-//        $handler = $this->createMock(RequestHandlerInterface::class);
-//
-//        $request = $requestFactory->createServerRequest('POST', '/some/uri')
-//            ->withHeader('Authorization', 'Bearer r32rf3rf32');
-//
-//        $exceptionMessage = 'Bad header';
-//        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
-//        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
-//            ->willThrowException(new ClientException(400, $exceptionMessage));
-//
-//        $this->expectException(HttpException::class);
-//        $this->expectExceptionMessage($exceptionMessage);
-//        $middleware = (new AuthMiddleware($tokenInfoRepository))
-//            ->withRequiredScopes(['events.example']);
-//        $middleware->process($request, $handler);
-//
-//        $exceptionMessage = 'Not found';
-//        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
-//        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
-//            ->willThrowException(new TokenInfoNotFoundException($exceptionMessage));
-//
-//        $middleware = (new AuthMiddleware($tokenInfoRepository))
-//            ->withRequiredScopes(['events.example']);
-//        $actual = $middleware->process($request, $handler);
-//
-//        $this->assertEquals(404, $actual->getStatusCode());
-//        $this->assertEquals($exceptionMessage, $actual->getReasonPhrase());
-//    }
-//
-//    public function testWithNotFoundException()
-//    {
-//        $requestFactory = new ServerRequestFactory();
-//        $handler = $this->createMock(RequestHandlerInterface::class);
-//
-//        $request = $requestFactory->createServerRequest('POST', '/some/uri')
-//            ->withHeader('Authorization', 'Bearer r32rf3rf32');
-//
-//        $exceptionMessage = 'Not found';
-//        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
-//        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
-//            ->willThrowException(new TokenInfoNotFoundException($exceptionMessage));
-//
-//        $middleware = (new AuthMiddleware($tokenInfoRepository))
-//            ->withRequiredScopes(['events.example']);
-//        $this->expectException(UnauthorizedException::class);
-//        $this->expectExceptionMessage($exceptionMessage);
-//        $middleware->process($request, $handler);
-//    }
-//
-//    public function testWithTokenInQuery(): void
-//    {
-//        $requestFactory = new ServerRequestFactory();
-//        $handler = $this->createMock(RequestHandlerInterface::class);
-//        $defaultSuccessResponse = $this->createMock(ResponseInterface::class);
-//
-//        $defaultSuccessResponse->method('getStatusCode')
-//            ->willReturn(200);
-//        $handler->method('handle')
-//            ->willReturn($defaultSuccessResponse);
-//
-//        $request = $requestFactory->createServerRequest('POST', '/some/uri')
-//            ->withQueryParams(['access_token' => 'r32rf3rf32']);
-//
-//        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
-//        $tokenInfoRepository->expects($this->once())->method('findByAccessToken')
-//            ->willReturn(new TokenInfo('', '', '', '', ['events' => []]));
-//
-//        $middleware = (new AuthMiddleware($tokenInfoRepository))
-//            ->withRequiredScopes(['events.example']);
-//        $actual = $middleware->process($request, $handler);
-//
-//        $this->assertEquals(200, $actual->getStatusCode());
-//    }
-//
-//    public function testWithTokenInBody(): void
-//    {
-//        $requestFactory = new ServerRequestFactory();
-//        $handler = $this->createMock(RequestHandlerInterface::class);
-//        $defaultSuccessResponse = $this->createMock(ResponseInterface::class);
-//
-//        $defaultSuccessResponse->method('getStatusCode')
-//            ->willReturn(200);
-//        $handler->method('handle')
-//            ->willReturn($defaultSuccessResponse);
-//
-//        $request = $requestFactory->createServerRequest('POST', '/some/uri')
-//            ->withParsedBody(['access_token' => 'r32rf3rf32']);
-//
-//        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
-//        $tokenInfoRepository->expects($this->once())->method('findByAccessToken')
-//            ->willReturn(new TokenInfo('', '', '', '', ['events.example' => []]));
-//        $middleware = (new AuthMiddleware($tokenInfoRepository))
-//            ->withRequiredScopes(['events.example']);
-//        $actual = $middleware->process($request, $handler);
-//
-//        $this->assertEquals(200, $actual->getStatusCode());
-//    }
+    public function testWithoutAuthorizationHeader(): void
+    {
+        $requestFactory = new ServerRequestFactory();
+        $request = $requestFactory->createServerRequest('POST', '/some/uri');
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
+        $middleware = new AuthMiddleware($tokenInfoRepository);
+
+        $this->expectException(UnauthorizedException::class);
+        $middleware->process($request, $handler);
+    }
+
+    public function testWithWrongScopes(): void
+    {
+        $requestFactory = new ServerRequestFactory();
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $request = $requestFactory->createServerRequest('POST', '/some/uri')
+            ->withHeader('Authorization', 'Bearer r32rf3rf32');
+        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
+        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
+            ->willReturn(new TokenInfo('', '', '', '', ['events.delivery' => []]));
+
+        $middleware = (new AuthMiddleware($tokenInfoRepository))
+            ->withRequiredScopes(['trips.example']);
+        $this->expectException(ForbiddenHttpException::class);
+        $middleware->process($request, $handler);
+    }
+
+    public function testWithGlobalScope(): void
+    {
+        $requestFactory = new ServerRequestFactory();
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $defaultSuccessResponse = $this->createMock(ResponseInterface::class);
+
+        $defaultSuccessResponse->method('getStatusCode')
+            ->willReturn(200);
+        $handler->method('handle')
+            ->willReturn($defaultSuccessResponse);
+
+        $request = $requestFactory->createServerRequest('POST', '/some/uri')
+            ->withHeader('Authorization', 'Bearer r32rf3rf32');
+
+        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
+        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
+            ->willReturn(new TokenInfo('', '', '', '', ['events' => []]));
+
+        $middleware = (new AuthMiddleware($tokenInfoRepository))
+            ->withRequiredScopes(['events.example']);
+        $actual = $middleware->process($request, $handler);
+
+        $this->assertEquals(200, $actual->getStatusCode());
+    }
+
+    public function testWithoutRequiredScopes(): void
+    {
+        $requestFactory = new ServerRequestFactory();
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $defaultSuccessResponse = $this->createMock(ResponseInterface::class);
+
+        $defaultSuccessResponse->method('getStatusCode')
+            ->willReturn(200);
+        $handler->method('handle')
+            ->willReturn($defaultSuccessResponse);
+
+        $request = $requestFactory->createServerRequest('POST', '/some/uri')
+            ->withHeader('Authorization', 'Bearer r32rf3rf32');
+
+        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
+        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
+            ->willReturn(new TokenInfo('', '', '', '', ['events' => []]));
+
+        $middleware = new AuthMiddleware($tokenInfoRepository);
+        $actual = $middleware->process($request, $handler);
+
+        $this->assertEquals(200, $actual->getStatusCode());
+    }
+
+    public function testWithClientException(): void
+    {
+        $requestFactory = new ServerRequestFactory();
+        $handler = $this->createMock(RequestHandlerInterface::class);
+
+        $request = $requestFactory->createServerRequest('POST', '/some/uri')
+            ->withHeader('Authorization', 'Bearer r32rf3rf32');
+
+        $exceptionMessage = 'Bad header';
+        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
+        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
+            ->willThrowException(new ClientException(400, $exceptionMessage));
+
+        $this->expectException(HttpException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+        $middleware = (new AuthMiddleware($tokenInfoRepository))
+            ->withRequiredScopes(['events.example']);
+        $middleware->process($request, $handler);
+
+        $exceptionMessage = 'Not found';
+        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
+        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
+            ->willThrowException(new TokenInfoNotFoundException($exceptionMessage));
+
+        $middleware = (new AuthMiddleware($tokenInfoRepository))
+            ->withRequiredScopes(['events.example']);
+        $actual = $middleware->process($request, $handler);
+
+        $this->assertEquals(404, $actual->getStatusCode());
+        $this->assertEquals($exceptionMessage, $actual->getReasonPhrase());
+    }
+
+    public function testWithNotFoundException()
+    {
+        $requestFactory = new ServerRequestFactory();
+        $handler = $this->createMock(RequestHandlerInterface::class);
+
+        $request = $requestFactory->createServerRequest('POST', '/some/uri')
+            ->withHeader('Authorization', 'Bearer r32rf3rf32');
+
+        $exceptionMessage = 'Not found';
+        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
+        $tokenInfoRepository->expects($this->once())->method('findByAuthorizationHeader')
+            ->willThrowException(new TokenInfoNotFoundException($exceptionMessage));
+
+        $middleware = (new AuthMiddleware($tokenInfoRepository))
+            ->withRequiredScopes(['events.example']);
+        $this->expectException(UnauthorizedException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+        $middleware->process($request, $handler);
+    }
+
+    public function testWithTokenInQuery(): void
+    {
+        $requestFactory = new ServerRequestFactory();
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $defaultSuccessResponse = $this->createMock(ResponseInterface::class);
+
+        $defaultSuccessResponse->method('getStatusCode')
+            ->willReturn(200);
+        $handler->method('handle')
+            ->willReturn($defaultSuccessResponse);
+
+        $request = $requestFactory->createServerRequest('POST', '/some/uri')
+            ->withQueryParams(['access_token' => 'r32rf3rf32']);
+
+        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
+        $tokenInfoRepository->expects($this->once())->method('findByAccessToken')
+            ->willReturn(new TokenInfo('', '', '', '', ['events' => []]));
+
+        $middleware = (new AuthMiddleware($tokenInfoRepository))
+            ->withRequiredScopes(['events.example']);
+        $actual = $middleware->process($request, $handler);
+
+        $this->assertEquals(200, $actual->getStatusCode());
+    }
+
+    public function testWithTokenInBody(): void
+    {
+        $requestFactory = new ServerRequestFactory();
+        $handler = $this->createMock(RequestHandlerInterface::class);
+        $defaultSuccessResponse = $this->createMock(ResponseInterface::class);
+
+        $defaultSuccessResponse->method('getStatusCode')
+            ->willReturn(200);
+        $handler->method('handle')
+            ->willReturn($defaultSuccessResponse);
+
+        $request = $requestFactory->createServerRequest('POST', '/some/uri')
+            ->withParsedBody(['access_token' => 'r32rf3rf32']);
+
+        $tokenInfoRepository = $this->createMock(TokenInfoRepository::class);
+        $tokenInfoRepository->expects($this->once())->method('findByAccessToken')
+            ->willReturn(new TokenInfo('', '', '', '', ['events.example' => []]));
+        $middleware = (new AuthMiddleware($tokenInfoRepository))
+            ->withRequiredScopes(['events.example']);
+        $actual = $middleware->process($request, $handler);
+
+        $this->assertEquals(200, $actual->getStatusCode());
+    }
 
     public function testUserAttributeSet()
     {
